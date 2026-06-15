fbc424a22bffbbf14b91ef9fe3ad3171ca336981
Bashmachok1982 <Bashmachok1982@gmail.com>
2026-06-15 18:34:46 +0300

    fix: rename Loading.module.css to lower case and add missing spinner animation classes

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import React from 'react';
-import styles from './Loading.module.css';
+import styles from './loading.module.css';
 
 export default function Loading(): React.ReactElement {
   return (
     <div className={styles.loaderContainer}>
-      {/* Чистый CSS Спинер, который не требует установки библиотек */}
+      {/* Чистий CSS Спінер, який не потребує встановлення сторонніх бібліотек */}
       <div className={styles.spinner}></div>
       <p className={styles.loaderText}>Loading, please wait...</p>
     </div>
