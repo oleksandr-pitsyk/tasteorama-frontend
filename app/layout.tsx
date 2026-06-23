470af3e07995b16d5bef185e0698ea73125bdd5a
Pitsyk Aleksandr <pitsyk.a@gmail.com>
2026-06-23 22:17:55 +0300

    fix metadata v.1.01

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export const metadata: Metadata = {
     siteName: 'Tasteorama',
     images: [
       {
-        url: 'favicon.svg',
+        url: 'favicon-without-background.svg',
         width: 1200,
         height: 630,
         alt: `Tasteorama picture`,
